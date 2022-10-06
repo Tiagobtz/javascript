9b2596acf3c30e0bb4a14108c8192f244616a8db
Tiago Henrique <tiagomacedoh015@gmail.com>
2022-10-05 22:17:43 -0300

    Atualizações

---
+++ Aprendizado/Exercícios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60){
+    console.log(`Você ultrapassou os 60Km/h. MULTADO!`)
+}
+console.log('Dirija sempre com cuidado!')
---
+++ Aprendizado/Exercícios/ex009/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+
+if (pais == 'Brasil'){
+    console.log(`Você mora no ${pais}, portanto você é brasileiro.`)
+}
+else{
+    console.log(`Você mora no ${pais}, portanto você é estrangeiro.`)
+}
