61f573ca86bb320ef5590f64c06e51103f9ef68f
Tiago Henrique <tiagomacedoh015@gmail.com>
2022-11-16 21:17:29 -0300

    atualizando

---
+++ Aprendizado/Exercícios/ambiente/teste.js
@@ -0,0 +1,14 @@
+/*Teste lógico de início*/
+
+/*var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+/*Teste lógico no final*/
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
